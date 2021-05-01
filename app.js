d93f8c666f4f20e35505f01ab1474730ec343ca6
tushar2k <tusharmittal2502@gmail.com>
2021-05-01 22:21:54 +0530

    Level 2 - Encrypt database using mongoose-encryption

---
+++ app.js
@@ -0,0 +1,90 @@
+//jshint esversion:6
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
+const encrypt = require("mongoose-encryption");
+
+const app = express();
+
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+mongoose.connect('mongodb://localhost:27017/userDB', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+});
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String
+});
+
+const secret = "Thisisourlittlesecret.";
+userSchema.plugin(encrypt, {secret: secret, encryptedFields:['password']});
+
+const User = mongoose.model("User", userSchema);
+
+app.get("/", function(req, res) {
+  res.render("home");
+});
+
+app.get("/login", function(req, res) {
+  res.render("login");
+});
+
+app.get("/register", function(req, res) {
+  res.render("register");
+});
+
+
+app.post("/register", function(req, res) {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password
+  });
+  newUser.save(function(err) {
+    if (!err) {
+      res.render("secrets");
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+app.post("/login", function(req, res) {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({
+    email: username
+  }, function(err, result) {
+    if (!err) {
+      if (result) {
+        if (result.password === password) {
+          res.render("secrets");
+        } else {
+          res.send("failed!");
+        }
+      } else {
+        res.send("Please register!");
+      }
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+
+
+
+
+
+
+
+
+app.listen(3000, function() {
+  console.log("listening on port 3000.");
+});
